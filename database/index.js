8d9c9d3b8613a0fec2bbc66b453c1ed407f35ee4
Phuc Tran <phuctranlv@gmail.com>
2020-03-07 10:14:58 -0800

    Modify repoSchema to have a unique field and create save funtion

--- database/index.js
+++ database/index.js
@@ -1,20 +1,47 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher', (error, client) => {
+  if (error) {
+  console.log('Connection error')
+  }
+});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log(`we're connected!!!`);
+});
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
-  name: String,
+  userName: String,
   avatar_url: String,
-  repos_url: String,
-  forks: Number
+  repo: String,
+  html_url: {
+    type: String,
+    unique: true
+  },
+  stargazers_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let create = (object) => {
+  Repo.create(object, (err, success) => {
+    if (err) {
+      console.log('there was a problem with creating a new document. Error message:', err);
+    } else {
+      console.log('successfully create new document. Success message:', success);
+    }
+  })
+}
+
+let save = (dataArray) => {
+  // Repo.find({}).exec((err, data) => {console.log(data)});
+  // Repo.deleteMany({}, (error, success)=> {error ? console.log('error:', error) : console.log('success:', success)});
+  console.log(`before the loop in database...`)
+  for (var i = 0; i < dataArray.length; i++) {
+    create(dataArray[i]);
+  }
 }
 
 module.exports.save = save;
