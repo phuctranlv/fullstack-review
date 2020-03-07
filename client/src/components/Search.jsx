5f361e3f0d5a6911177da4a590f9308ad8fdb3ea
Phuc Tran <phuctranlv@gmail.com>
2020-03-06 16:21:17 -0800

    Fix search method

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       term: ''
-    }
+    };
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
   }
 
   onChange (e) {
@@ -14,16 +17,28 @@ class Search extends React.Component {
     });
   }
 
-  search() {
-    this.props.onSearch(this.state.term);
+  search(e) {
+    console.log(`${this.state.term} was searched`);
+    $.ajax({
+      type: 'post',
+      url: 'http://localhost:1128/repos',
+      data: {query: this.state.term},
+      success: (data) => {
+        $.ajax({
+          type: 'get',
+          url: 'http://localhost:1128/repos',
+          success: this.props.onSearch(data)
+        })
+      }
+    })
   }
 
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 
