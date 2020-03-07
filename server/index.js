970ef11bb99151863633d7b47791f716564ff395
Phuc Tran <phuctranlv@gmail.com>
2020-03-06 16:22:44 -0800

    Complete post's api request to github functionality

--- server/index.js
+++ server/index.js
@@ -1,18 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const getReposByUsername = require('../helpers/github').getReposByUsername;
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
-  // save the repo information in the database
+  getReposByUsername(req.body.query, (err, result) => {
+    if (err) {
+      console.log('In server index.js, running post request gives error:', err);
+    } else {
+      console.log('result from searching github:', JSON.parse(result.body));
+      res.sendStatus(200);
+    }
+    // save the repo information in the database
+    let responseFromGithub = JSON.parse(result.body);
+    let dataToStoreInDatabase = {};
+    dataToStoreInDatabase.name = responseFromGithub[0].owner.login;
+    dataToStoreInDatabase.avatar_url = responseFromGithub[0].owner.avatar_url;
+    dataToStoreInDatabase.repos_url = responseFromGithub[0].owner.repos_url;
+    dataToStoreInDatabase.forks = responseFromGithub[0].forks;
+    console.log('dataToStoreInDatabase:', dataToStoreInDatabase);
+  })
+
+
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  console.log('in index.js get request received');
+  res.end(JSON.stringify(['hello']));
 });
 
 let port = 1128;
