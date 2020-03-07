92de251039fe6247569751da5ec7998e3f1b6233
Phuc Tran <phuctranlv@gmail.com>
2020-03-07 14:45:08 -0800

    Clean out console logs

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,19 +14,16 @@ class App extends React.Component {
   }
 
   search (data) {
-    // TODO
-    console.log('new list of top 25 repos from sending a post request:', data);
     this.setState({
       repos: data
-    })
+    });
   }
 
   render () {
-    console.log('this.state in App:', this.state);
     return (<div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
       <Search onSearch={this.search.bind(this)}/>
+      <RepoList repos={this.state.repos}/>
     </div>)
   }
 }
@@ -35,7 +32,6 @@ $.ajax({
   type: 'get',
   url: 'http://localhost:1128/repos',
   success: (result) => {
-    console.dir('result from initial load get request:', JSON.parse(result));
     ReactDOM.render(<App repos={JSON.parse(result)} />, document.getElementById('app'));
   }
 })
