a8394158ddb66fadda533b4d82540ff457555e64
Phuc Tran <phuctranlv@gmail.com>
2020-03-06 16:20:36 -0800

    Add initial get request and define search method

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,18 +7,22 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      repos: []
+    this.state = {
+      repos: this.props.repos
     }
 
   }
 
-  search (term) {
-    console.log(`${term} was searched`);
+  search (data) {
     // TODO
+    console.log('new list of top 25 repos from sending a post request:', data);
+    this.setState({
+      repos: data
+    })
   }
 
   render () {
+    console.log('this.state in App:', this.state);
     return (<div>
       <h1>Github Fetcher</h1>
       <RepoList repos={this.state.repos}/>
@@ -27,4 +31,12 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+$.ajax({
+  type: 'get',
+  url: 'http://localhost:1128/repos',
+  success: (result) => {
+    console.dir('result from initial load get request:', JSON.parse(result));
+    ReactDOM.render(<App repos={JSON.parse(result)} />, document.getElementById('app'));
+  }
+})
+
