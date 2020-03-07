89a28abe8e493ea63cdc05e445d02d04affb3b67
Phuc Tran <phuctranlv@gmail.com>
2020-03-07 14:46:10 -0800

    Clean out console logs

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const getReposByUsername = require('../helpers/github').getReposByUsername;
 const save = require('../database/index').save;
+const findReposWithUsername = require('../database/index').findReposWithUsername;
 
 let app = express();
 
@@ -10,18 +11,13 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
   getReposByUsername(req.body.query, (err, result) => {
     if (err) {
       console.log('In server index.js, running post request gives error:', err);
     } else {
       res.sendStatus(200);
     }
-    // save the repo information in the database
     let responseFromGithub = JSON.parse(result.body);
-    // console.log('responseFromGithub:', responseFromGithub);
     let dataArrayToSaveToDatabase = [];
     for (var i = 0; i < responseFromGithub.length; i++) {
       let dataObject = {};
@@ -33,17 +29,24 @@ app.post('/repos', function (req, res) {
       dataArrayToSaveToDatabase.push(dataObject);
     }
     save(dataArrayToSaveToDatabase);
-
   })
 
 
 });
 
 app.get('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
-  console.log('in index.js get request received');
-  res.end(JSON.stringify(['hello']));
+  findReposWithUsername((error, result) => {
+    if (error) {
+      console.log(`Error in server's get request: ${error}`);
+      res.status(400).send(error);
+    } else {
+      let sortedResult = result.sort((a, b) => {
+        return b.stargazers_count - a.stargazers_count;
+      });
+      let top25 = sortedResult.slice(0, 25);
+      res.send(JSON.stringify(top25));
+    }
+  })
 });
 
 let port = 1128;
